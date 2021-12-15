4ab436a5312d0e403d80cfccb8a3bec2376f1869
Allan de Oliveira Almeida <techall@hotmail.com.br>
2021-12-15 07:51:20 -0300

    desenvolvimento do corpo do robo finalizado

--- automationViteApp.js
+++ automationViteApp.js
@@ -1,6 +1,44 @@
 const webdriver = require('selenium-webdriver');
-const {By} = require('selenium-webdriver');
-const { debug } = require('util');
+const { By } = require('selenium-webdriver');
+
+var camposFormulario = [];
+
+async function montaBase() {
+
+    global.camposFormulario
+    const gerenciadorArquivos = require('fs')
+    var linhas = ''
+
+    try{
+
+        arquivo = gerenciadorArquivos.readFileSync('./baseDados/base_dados_13.12.2021.csv', 'utf8')
+
+    } catch(error) {
+
+        console.log(error)
+
+    }
+
+    linhas = arquivo.split('\r\n')
+
+    for (var i = 1; i < linhas.length; i++) {
+
+        colunas = linhas[i].split(',')
+
+        camposFormulario.push({
+
+            nome: colunas[0],
+            rg: colunas[1],
+            cpf: colunas[2],
+            dataNascimento: gerarData(colunas[3]),
+            dataAdmissao: gerarData(colunas[4]),
+            cargo: colunas[5]
+
+        });
+
+    }    
+
+}
 
 async function abrir_navegador() {
 
@@ -16,47 +54,65 @@ async function pegar_url() {
 
 async function executar_script() {
 
-    var camposFormulario = [ "nome", "rg", "cpf", "dataNascimento", "dataAdmissao", "cargo" ];
+    const actions = navegador.actions();
 
-    camposFormulario.push( {
-        nome : '',
-        rg : '',
-        cpf : '',
-        dataNascimento : '',
-        dataAdmissao : '',
-        cargo : ''
-    } );
 
-    let nome = navegador.findElement( By.xpath( "(*//label[contains(text(), 'Nome completo')])[1]" ) );
-    let rg = navegador.findElement( By.xpath( "(*//label[contains(text(), 'RG')])[1]" ) );
-    let cpf = navegador.findElement( By.xpath( "(*//label[contains(text(), 'CPF')])[1]" ) );
-    let dataNascimento = navegador.findElement( By.xpath( "(*//label[contains(text(), 'Data de nascimento')])[1]" ) );
-    let dataAdmissao = navegador.findElement( By.xpath( "(*//label[contains(text(), 'Data de admissão')])[1]" ) );
-    let cargo = navegador.findElement( By.xpath( "(*//label[contains(text(), 'Cargo')])[1]" ) );
+    for (var i = 0; i < camposFormulario.length; i++) {
 
-    nome.click()
-    nome.sendKeys('qualquer nome')
-    sleep(2000)
+        var nome = await encontrarElementoPorID("nameInput", 1)
+        var rg = await encontrarElementoPorID("rgInput", 1)
+        var cpf = await encontrarElementoPorID("cpfInput", 1)
+        var dataNascimento = await encontrarElementoPorID("birthdayInput", 1)
+        var dataAdmissao = await encontrarElementoPorID("admissionInput", 1)
+        var cargo = await encontrarElementoPorID("jobroleInput", 1)
 
-    rg.click()
-    rg.sendKeys('444444444')
-    sleep(2000)
+        nome.click()
+        nome.sendKeys(camposFormulario[i].nome)
+        await navegador.sleep(300)
 
-    cpf.click()
-    cpf.sendKeys('99999999999')
-    sleep(2000)
+        rg.click()
+        rg.sendKeys(camposFormulario[i].rg)
+        await navegador.sleep(300)
 
-    dataNascimento.click()
-    dataNascimento.sendKeys('11/11/1111')
-    sleep(2000)
+        cpf.click()
+        cpf.sendKeys(camposFormulario[i].cpf)
+        await navegador.sleep(300)
 
-    dataAdmissao.click()
-    dataAdmissao.sendKeys('22/22/2222')
-    sleep(2000)
+        await preencherData( dataNascimento, camposFormulario[i].dataNascimento )
+        await navegador.sleep( 1000 )
+        
+        await preencherData( dataAdmissao, camposFormulario[i].dataAdmissao )
+        await navegador.sleep(1000)
+        
+        cargo.click()
+        cargo.sendKeys(camposFormulario[i].cargo)
+        await navegador.sleep(300)
 
-    cargo.click()
-    cargo.sendKeys('qualquer um')
-    sleep(2000)
+        var botaoSalvar = encontrarElementoPorXPath("(//button[contains(text(), '')])[2]")
+        botaoSalvar.click()
+        await navegador.sleep(1000)
+
+    }
+
+    function preencherData(elemento, dado) {
+
+        actions.click(elemento).sendKeys('12212021').perform();
+        actions.click(elemento).keyDown(webdriver.Key.ESCAPE).keyUp(webdriver.Key.ESCAPE).perform();
+        navegador.sleep(1000);
+
+    }
+
+    function encontrarElementoPorID(id, indice) {
+        return navegador.findElement(By.xpath(`//input[@id="${id}"][${indice}]`));
+    }
+
+    function encontrarElementoPorTexto(tag, texto, indice) {
+        return navegador.findElement(By.xpath(`(//${tag}[contains(text(), '${texto}')])[${indice}]`))
+    }
+
+    function encontrarElementoPorXPath(caminho) {
+        return navegador.findElement(By.xpath(caminho))
+    }
 
 }
 
@@ -66,12 +122,21 @@ async function fechar_navegador() {
         }
     )
 }
+
 async function error() {
     return console.log('Oops!')
 }
 
+function gerarData(data) {
+    var date = new Date(data);
+    date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    return date
+}
+
+
 abrir_navegador()
     .catch(error)
+    .then(montaBase)
     .then(pegar_url)
     .then(executar_script)
     .finally(fechar_navegador)
